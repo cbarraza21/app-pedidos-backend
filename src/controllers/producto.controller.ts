3c8d5a4d03b44773e60cb0c3e6082b01504ce5ef
cbarraza21 <cbarrazar219@gmail.com>
2021-12-11 12:51:47 -0500

    Modificacion de productos auth

--- src/controllers/producto.controller.ts
+++ src/controllers/producto.controller.ts
@@ -15,13 +15,14 @@ import {
 import {Producto} from '../models';
 import {ProductoRepository} from '../repositories';
 
+@authenticate("admin")
 export class ProductoController {
   constructor(
     @repository(ProductoRepository)
     public productoRepository: ProductoRepository,
   ) { }
 
-  @authenticate("admin")
+
   @post('/productos')
   @response(200, {
     description: 'Producto model instance',
@@ -43,6 +44,7 @@ export class ProductoController {
     return this.productoRepository.create(producto);
   }
 
+  @authenticate.skip()
   @get('/productos/count')
   @response(200, {
     description: 'Producto model count',
@@ -54,6 +56,7 @@ export class ProductoController {
     return this.productoRepository.count(where);
   }
 
+  @authenticate.skip()
   @get('/productos')
   @response(200, {
     description: 'Array of Producto model instances',
@@ -91,6 +94,7 @@ export class ProductoController {
     return this.productoRepository.updateAll(producto, where);
   }
 
+  @authenticate.skip()
   @get('/productos/{id}')
   @response(200, {
     description: 'Producto model instance',
@@ -125,6 +129,7 @@ export class ProductoController {
     await this.productoRepository.updateById(id, producto);
   }
 
+  @authenticate.skip()
   @put('/productos/{id}')
   @response(204, {
     description: 'Producto PUT success',
